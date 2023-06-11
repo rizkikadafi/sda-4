167199d898acf32dbc22f2191906b7eb2acc6fe5
Muhamad Rizki Kadafi <rizkikadafi11@gmail.com>
2023-06-11 16:51:23 +0700

    mengerjakan grid display bst

--- apps/data_structure/bst.py
+++ apps/data_structure/bst.py
@@ -1,135 +1,78 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
+class BstNode:
+    def __init__(self, key):
+        self.key = key
         self.right = None
         self.left = None
 
-
-class BST:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, value):
-        if self.root is None:
-            self.root = Node(value)
-        else:
-            self._insert_recursive(value, self.root)
-
-    def _insert_recursive(self, value, current_node: Node):
-        if value <= current_node.value:
-            if current_node.left is None:
-                current_node.left = Node(value)
+    def insert(self, key):
+        if self.key == key:
+            return
+        elif self.key < key:
+            if self.right is None:
+                self.right = BstNode(key)
             else:
-                self._insert_recursive(value, current_node.left)
-        elif value > current_node.value:
-            if current_node.right is None:
-                current_node.right = Node(value)
+                self.right.insert(key)
+        else:  # self.key > key
+            if self.left is None:
+                self.left = BstNode(key)
             else:
-                self._insert_recursive(value, current_node.right)
+                self.left.insert(key)
 
     def display(self):
-        pass
+        lines, *_ = self._display_aux()
+        for line in lines:
+            print(line)
 
-    def _display_aux(self, current_node: Node):
-        if current_node.left is None and current_node.right is Node:
-            len_digit = len(str(current_node.value))
+    def _display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = "%s" % self.key
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
 
-            return len_digit
-        elif current_node.left is None:
-            data_acc = self._display_aux(current_node.right)
-            return data_acc
-        elif current_node.right is None:
-            data_acc = self._display_aux(current_node.left)
-            return data_acc
-        else:
-            right_data = self._display_aux(current_node.right)
-            left_data = self._display_aux(current_node.left)
+        # Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = "%s" % self.key
+            u = len(s)
+            first_line = (x + 1) * " " + (n - x - 1) * "_" + s
+            second_line = x * " " + "/" + (n - x - 1 + u) * " "
+            shifted_lines = [line + u * " " for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
 
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = "%s" % self.key
+            u = len(s)
+            first_line = s + x * "_" + (n - x) * " "
+            second_line = (u + x) * " " + "\\" + (n - x - 1) * " "
+            shifted_lines = [u * " " + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
 
-if __name__ == "__main__":
-    bst = BST()
-    data = [5, 1, 4, 3, 2]
-    for d in data:
-        bst.insert(d)
-
-
-# class BstNode:
-#     def __init__(self, key):
-#         self.key = key
-#         self.right = None
-#         self.left = None
-
-#     def insert(self, key):
-#         if self.key == key:
-#             return
-#         elif self.key < key:
-#             if self.right is None:
-#                 self.right = BstNode(key)
-#             else:
-#                 self.right.insert(key)
-#         else:  # self.key > key
-#             if self.left is None:
-#                 self.left = BstNode(key)
-#             else:
-#                 self.left.insert(key)
-
-#     def display(self):
-#         lines, *_ = self._display_aux()
-#         for line in lines:
-#             print(line)
-
-#     def _display_aux(self):
-#         """Returns list of strings, width, height, and horizontal coordinate of the root."""
-#         # No child.
-#         if self.right is None and self.left is None:
-#             line = "%s" % self.key
-#             width = len(line)
-#             height = 1
-#             middle = width // 2
-#             return [line], width, height, middle
-
-#         # Only left child.
-#         if self.right is None:
-#             lines, n, p, x = self.left._display_aux()
-#             s = "%s" % self.key
-#             u = len(s)
-#             first_line = (x + 1) * " " + (n - x - 1) * "_" + s
-#             second_line = x * " " + "/" + (n - x - 1 + u) * " "
-#             shifted_lines = [line + u * " " for line in lines]
-#             return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
-
-#         # Only right child.
-#         if self.left is None:
-#             lines, n, p, x = self.right._display_aux()
-#             s = "%s" % self.key
-#             u = len(s)
-#             first_line = s + x * "_" + (n - x) * " "
-#             second_line = (u + x) * " " + "\\" + (n - x - 1) * " "
-#             shifted_lines = [u * " " + line for line in lines]
-#             return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
-
-#         # Two children.
-#         left, n, p, x = self.left._display_aux()
-#         right, m, q, y = self.right._display_aux()
-#         s = "%s" % self.key
-#         u = len(s)
-#         first_line = (x + 1) * " " + (n - x - 1) * "_" + s + y * "_" + (m - y) * " "
-#         second_line = (
-#             x * " " + "/" + (n - x - 1 + u + y) * " " + "\\" + (m - y - 1) * " "
-#         )
-#         if p < q:
-#             left += [n * " "] * (q - p)
-#         elif q < p:
-#             right += [m * " "] * (p - q)
-#         zipped_lines = zip(left, right)
-#         lines = [first_line, second_line] + [a + u * " " + b for a, b in zipped_lines]
-#         return lines, n + m + u, max(p, q) + 2, n + u // 2
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = "%s" % self.key
+        u = len(s)
+        first_line = (x + 1) * " " + (n - x - 1) * "_" + s + y * "_" + (m - y) * " "
+        second_line = (
+            x * " " + "/" + (n - x - 1 + u + y) * " " + "\\" + (m - y - 1) * " "
+        )
+        if p < q:
+            left += [n * " "] * (q - p)
+        elif q < p:
+            right += [m * " "] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * " " + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
 
 
-# import random
-
-# b = BstNode(5)
-# data = [3, 1, 2, 4]
-# for d in data:
-#     b.insert(d)
-# b.display()
+b = BstNode(10)
+data = [5, 6, 7, 12, 11, 8]
+for d in data:
+    b.insert(d)
+b.display()
--- apps/program4.py
+++ apps/program4.py
@@ -1,26 +1,120 @@
 from utils.app import *
 
-chars = ["┘", "┌", "─", "└", "┐", "┴"]
+chars = {
+    "up_left": "┘",
+    "up_right": "└",
+    "down_left": "┌",
+    "down_right": "┐",
+    "middle": "┴",
+    "strip": "─",
+}
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert_recursive(value, self.root)
+
+    def _insert_recursive(self, value, current_node: Node):
+        if value <= current_node.value:
+            if current_node.left is None:
+                current_node.left = Node(value)
+            else:
+                self._insert_recursive(value, current_node.left)
+        elif value > current_node.value:
+            if current_node.right is None:
+                current_node.right = Node(value)
+            else:
+                self._insert_recursive(value, current_node.right)
+
+    def display(self):
+        grid = Table.grid()
+
+        rows, *_ = self._display_aux(self.root)
+        for row in rows:
+            grid.add_row(*row)
+        return grid
+
+    def _display_aux(self, current_node: Node):
+        if current_node.left is None and current_node.right is None:
+            row = [str(current_node.value)]
+            height = 1
+            len_col_subtree = [1, 1]
+            middle = len(str(current_node.value)) // 2
+            return [row], height, middle, len_col_subtree
+
+        elif current_node.left is None:
+            (rows, height, middle_down, len_col_subtree) = self._display_aux(
+                current_node.right
+            )
+            middle_up = len(str(current_node.value)) // 2
+            rows = [[""] + row for row in rows]
+            first_row = [str(current_node.value)]
+            second_row = (
+                [" " * middle_up + chars["up_right"] + chars["strip"] * middle_up]
+                + [chars["strip"]] * (abs(len_col_subtree[0] - len_col_subtree[1]))
+                + [(chars["strip"] * middle_down) + chars["down_right"]]
+            )
+            len_col_subtree[1] += 1
+            return (
+                [first_row, second_row] + rows,
+                height + 1,
+                middle_up,
+                len_col_subtree,
+            )
+        elif current_node.right is None:
+            rows, height, middle_down, len_col_subtree = self._display_aux(
+                current_node.left
+            )
+            middle_up = len(str(current_node.value)) // 2
+            first_row = [""] * (len_col_subtree[0] + len_col_subtree[1] - 1) + [
+                str(current_node.value)
+            ]
+            second_row = (
+                [" " * middle_down + chars["down_left"] + chars["strip"] * middle_down]
+                + [chars["strip"]] * (len_col_subtree[1])
+                + [chars["strip"] * middle_up + chars["up_left"]]
+            )
+            len_col_subtree[0] += 1
+            return (
+                [first_row, second_row] + rows,
+                height + 1,
+                middle_up,
+                len_col_subtree,
+            )
+        else:
+            left_rows, height_l, middle_dl, len_col_subtree_l = self._display_aux(
+                current_node.left
+            )
+            right_rows, height_r, middle_dr, len_col_subtree_r = self._display_aux(
+                current_node.left
+            )
+
+            return rows
 
 
 def main():
-    grid = Table.grid(padding=(0, 0))
+    # data = [5, 4, 3]
+    # data = [3, 444, 555]
+    data = [10, 5, 7, 9, 8]
+    # data = [8, 12, 10, 9]
+    bst = BST()
+    for d in data:
+        bst.insert(d)
 
-    x = [Align(str(i), align="center") for i in range(1, 21)]
-    grid.add_row("", Align("20", align="center"), "")
-    grid.add_row(
-        Align(" ┌─", align="center"),
-        Align("─" * 5 + "┴" + "─" * 5, align="center"),
-        "┐",
-    )
-    grid.add_row(Align("5", align="center"), "", Align("25", align="center"))
-    grid.add_row(
-        Align("", align="center"),
-        Align("─" * 2, align="right"),
-        Align("┘", align="center"),
-    )
-
-    console.print(grid, justify="center")
+    console.print(bst.display(), justify="center")
     if Confirm.ask("\n[bold]Keluar Program"):
         return program4.stop()
 
